b71cfc0e1bed8a0373b073f3568a0df94f2028a9
Brandon Karl <brandon@bkarl.com>
2018-03-23 11:29:56 -0400

    many updates

--- routes/likes.js
+++ routes/likes.js
@@ -29,4 +29,26 @@ router.post('/', async (req, res) => {
   return res.json(newLike.rows[0])
 })
 
+router.delete('/:user_id/:post_id', async (req, res) => {
+  const { user_id, post_id } = req.params
+  try {
+    await db.query(
+      `DELETE FROM likes WHERE user_id = $1 AND post_id = $2`,
+      [user_id, post_id]
+    )
+  } catch(e) {
+    return res.status(400).json({ error: 'Could not remove like' })
+  }
+
+  try {
+    await db.query(
+      'UPDATE posts SET likes = likes - 1 WHERE id = $1', [post_id]
+    )
+  } catch(e) {
+    return res.status(400).json({ error: 'Could not remove like' })
+  }
+
+  return res.send('removed')
+})
+
 export default router
--- routes/users.js
+++ routes/users.js
@@ -13,8 +13,10 @@ const router = express.Router()
 router.get('/:id', async (req, res) => {
   const { id } = req.params
   try {
-    var user = await db.query(
-      `SELECT id, firstname, lastname, email, created_at FROM users WHERE id = $1`,
+    var user = await db.query(`
+      SELECT id, firstname, lastname, email, created_at, bio, username, profile_picture
+      FROM users 
+      WHERE id = $1`,
       [id]
     )
   } catch(e) {
@@ -26,13 +28,44 @@ router.get('/:id', async (req, res) => {
   }
 
   try {
+    var followerCount = await db.query(`
+      SELECT count(followee)
+      FROM follows 
+      WHERE followee = $1`,
+      [id]
+    )
+  } catch(e) {
+    return res.status(400).json({ error: 'Can not get number of followers' })
+  }
+
+  try {
+    var followingCount = await db.query(`
+      SELECT count(follower)
+      FROM follows 
+      WHERE follower = $1`,
+      [id]
+    )
+  } catch(e) {
+    return res.status(400).json({ error: 'Can not get number of number of following' })
+  }
+
+  user.rows[0]['followers'] = followerCount.rows[0].count
+  user.rows[0]['following'] = followingCount.rows[0].count
+
+  return res.json({ ...user.rows[0] })
+})
+
+router.get('/:id/feed', async (req, res) => {
+  const { id } = req.params
+  try {
     var feed = await db.query(`
     SELECT firstname, lastname, email, users.created_at AS user_created_at, users.id AS user_id,
       posts.created_at, image_url, posts.id, content, likes 
     FROM follows 
     JOIN posts ON posts.user_id = followee
     JOIN users ON users.id = posts.user_id
-    WHERE follower = $1`,
+    WHERE follower = $1
+    ORDER BY posts.id DESC`,
     [id])
   } catch(e) {
     return res.status(500).json({ error: 'Error getting feed for user' })
@@ -55,7 +88,71 @@ router.get('/:id', async (req, res) => {
     }
   })
 
-  return res.json({ ...user.rows[0], feed })
+  return res.json(feed)
+})
+
+router.get('/:id/posts', async (req, res) => {
+  const { id } = req.params
+  try {
+    var feed = await db.query(`
+    SELECT id, content, image_url, likes, user_id, created_at
+    FROM posts
+    WHERE user_id = $1
+    ORDER BY id DESC`,
+    [id])
+  } catch(e) {
+    return res.status(500).json({ error: 'Error getting posts for user' })
+  }
+
+  return res.json(feed.rows)
+})
+
+router.get('/:id/followers', async (req, res) => {
+  const { id } = req.params
+  try {
+    var followers = await db.query(`
+    SELECT id, firstname, lastname, email, users.created_at, bio, username, profile_picture
+    FROM follows
+    JOIN users ON users.id = follower
+    WHERE followee = $1`,
+    [id])
+  } catch(e) {
+    return res.status(500).json({ error: 'Error getting posts for user' })
+  }
+
+  return res.json(followers.rows)
+})
+
+router.get('/:id/following', async (req, res) => {
+  const { id } = req.params
+  try {
+    var followers = await db.query(`
+    SELECT id, firstname, lastname, email, users.created_at, bio, username, profile_picture
+    FROM follows
+    JOIN users ON users.id = followee
+    WHERE follower = $1`,
+    [id])
+  } catch(e) {
+    return res.status(500).json({ error: 'Error getting posts for user' })
+  }
+
+  return res.json(followers.rows)
+})
+
+router.get('/search/:username', async (req, res) => {
+  const { username } = req.params
+  console.log(username)
+  try {
+    var users = await db.query(`
+    SELECT id, firstname, lastname, email, created_at, bio, username, profile_picture
+    FROM users
+    WHERE username LIKE $1`,
+    [`%${username}%`])
+  } catch(e) {
+    return res.status(500).json({ error: 'Error getting posts for user' })
+  }
+
+  res.json(users.rows)
 })
 
 router.post('/', async (req, res) => {
